a43c0f5f46ea55eafe243e933b98b2c54363f38d
case <case@casesandberg.com>
2016-02-24 23:42:06 -0600

    Remove Semicolons

--- src/react-map-styles.js
+++ src/react-map-styles.js
@@ -1,29 +1,29 @@
-'use strict';
+'use strict'
 
 module.exports = (file) => {
 
   if (this && this.cacheable) {
-    this.cacheable();
+    this.cacheable()
   }
 
-  let isProp;
+  let isProp
   while (isProp = /(is=(?:"|')(.+?)(?:"|'))/.exec(file)) {
     // The is block that should be replaced. Looks like: `is="message"`
-    let prop = isProp[1];
+    let prop = isProp[1]
 
     // The name of the value that we want to be returning from the style function.
-    let elementName = isProp[2];
+    let elementName = isProp[2]
 
     // if it is capitalized, set spread
     if (elementName[0] === elementName[0].toUpperCase()) {
-      file = file.replace(prop, `{...this.styles().${ elementName }}`);
+      file = file.replace(prop, `{...this.styles().${ elementName }}`)
     }
 
     // otherwise just give it an inline style
     else {
-      file = file.replace(prop, `style={ this.styles().${ elementName } }`);
+      file = file.replace(prop, `style={ this.styles().${ elementName } }`)
     }
   }
 
-  return file;
-};
+  return file
+}
